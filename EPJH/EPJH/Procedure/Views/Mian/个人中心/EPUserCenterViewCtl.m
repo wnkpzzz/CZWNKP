79c66741a68a604477fb5a911739f7b976acdd0d
Hans <hans3d@HansdeMac-mini.local>
2020-08-17 14:53:12 +0800

    1

--- EPJH/EPJH/Procedure/Views/Mian/个人中心/EPUserCenterViewCtl.m
+++ EPJH/EPJH/Procedure/Views/Mian/个人中心/EPUserCenterViewCtl.m
@@ -10,9 +10,16 @@
 
 @interface EPUserCenterViewCtl ()
 
+@property (weak, nonatomic) IBOutlet UIImageView *headImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *sexImgView;
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *phoneLabel;
+
 @end
 
 @implementation EPUserCenterViewCtl
+
+#pragma mark - 生命周期
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -21,14 +28,77 @@
     self.navigationItem.title = @"个人中心";
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+     
 }
-*/
+
+- (void)loadBaseConfig{
+}
+
+#pragma mark - 点击事件
+
+// 个人中心
+- (IBAction)userInfoAction:(id)sender {
+    
+     
+}
+
+// 云备份
+- (IBAction)dataCenterAction:(id)sender {
+     
+     
+}
+
+// 案例分享
+- (IBAction)caseShareAction:(id)sender {
+    
+     
+}
+
+// 修改密码
+- (IBAction)changePwdAction:(id)sender {
+    
+}
+
+// 帮助中心
+- (IBAction)helpAction:(id)sender {
+     
+ 
+}
+
+// 意见反馈
+- (IBAction)feedbackAction:(id)sender {
+    
+    
+}
+
+// 联系客服
+- (IBAction)callPhoneAction:(id)sender {
+     
+ 
+}
+ 
+// 用户投诉
+- (IBAction)callComplaintAction:(id)sender{
+ 
+    
+}
+
+// 关于我们
+- (IBAction)aboutUsAction:(id)sender {
+ 
+    
+}
+
+// 退出登录
+- (IBAction)exitAction:(id)sender {
+    
+}
+
+// 注销账号
+- (IBAction)cancellationAccountAction:(id)sender {
+    
+}
 
 @end
